5a90dd01245b4fe74fcbc248e7449234d83294d9
Sherman Au <shermau13@gmail.com>
2022-04-27 02:58:08 -0600

    feat: .discount_amount

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -10,13 +10,22 @@ class Invoice < ApplicationRecord
   enum status: {"Cancelled" => 0, "In Progress" => 1, "Completed" => 2}
 
   def total_revenue
-    invoice_items.sum("invoice_items.quantity * invoice_items.unit_price")
+    invoice_items.sum("quantity * unit_price")
   end
 
   def self.incomplete_invoices
     joins(:invoice_items)
-      .where("invoice_items.status = 0 OR invoice_items.status = 1")
+      .where(invoice_items: {status: [0, 1]})
       .group("invoices.id")
       .order(created_at: :asc)
+  end
+
+  def discount_amount
+    invoice_items.joins(:bulk_discounts)
+    .select("invoice_items.*, max(invoice_items.quantity * (invoice_items.unit_price * (bulk_discounts.percentage / 100.0))) as discount")
+    .where("items.merchant_id = bulk_discounts.merchant_id")
+    .where("invoice_items.quantity >= bulk_discounts.threshold")
+    .group(:id)
+    .sum(&:discount)
   end
 end
