a2b2d221b1a0b09b20e343a8c687deeafcaa4506
Sherman Au <shermau13@gmail.com>
2022-04-26 19:37:26 -0600

    feat: add .new + .create to controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,4 +2,26 @@ class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @merchant.bulk_discounts.create!(discount_params)
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
+  private
+
+  def discount_params
+    params.permit(:percentage, :threshold)
+  end
+
 end
