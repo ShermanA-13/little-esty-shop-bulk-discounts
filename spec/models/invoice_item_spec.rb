b3b2bde971c9374a1ca2f0a4137ccdf427e65178
Sherman Au <shermau13@gmail.com>
2022-04-27 02:57:04 -0600

    test: .discount_amount

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -15,6 +15,6 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to(:invoice) }
     it { should belong_to(:item) }
     it { should have_one(:merchant).through(:item) }
-    it { should have_many(:bulk_discounts) }
+    it { should have_many(:bulk_discounts).through(:merchant) }
   end
 end
