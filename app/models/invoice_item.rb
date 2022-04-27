2b96dc2bed980ed167b68f42f3b7cc7ff624d2fe
Sherman Au <shermau13@gmail.com>
2022-04-27 01:52:25 -0600

    feat: bulk_discount relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_one :merchant, through: :item
+  has_many :bulk_discounts, through: :merchant
 
   validates :quantity, presence: true
   validates :unit_price, presence: true, numericality: true
