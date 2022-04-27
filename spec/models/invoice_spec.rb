425542afabdb389f6feeef0c858a202b78603a0e
Sherman Au <shermau13@gmail.com>
2022-04-27 03:11:37 -0600

    refactor: grouping for dev empathy

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -20,14 +20,18 @@ RSpec.describe Invoice, type: :model do
   describe "methods" do
     before do
       @merchant_1 = create :merchant
-      @merchant_2 = create :merchant
-      @customer = create(:customer)
-
       @item_1 = create :item, {merchant_id: @merchant_1.id}
       @item_2 = create :item, {merchant_id: @merchant_1.id}
+      @bulk_1 = @merchant_1.bulk_discounts.create!(percentage: 10, threshold: 10)
+      @bulk_2 = @merchant_1.bulk_discounts.create!(percentage: 25, threshold: 15)
+      @bulk_3 = @merchant_1.bulk_discounts.create!(percentage: 15, threshold: 20)
+
+      @merchant_2 = create :merchant
       @item_3 = create :item, {merchant_id: @merchant_2.id}
       @item_4 = create :item, {merchant_id: @merchant_1.id}
+      @bulk_4 = @merchant_2.bulk_discounts.create!(percentage: 10, threshold: 6)
 
+      @customer = create(:customer)
 
       @invoice_1 = create(:invoice, customer_id: @customer.id)
       @invoice_item_1 = create(:invoice_item, invoice_id: @invoice_1.id, item_id: @item_4.id, quantity: 16, unit_price: 2200)
@@ -38,15 +42,8 @@ RSpec.describe Invoice, type: :model do
       @invoice_2 = create(:invoice, customer_id: @customer.id)
       @invoice_item_5 = create(:invoice_item, invoice_id: @invoice_2.id, item_id: @item_3.id, quantity: 9, unit_price: 7300 )
 
-
       @invoice_3 = create(:invoice, customer_id: @customer.id)
       @invoice_item_6 = create(:invoice_item, invoice_id: @invoice_3.id, item_id: @item_2.id, quantity: 9, unit_price: 9500, status: 2)
-
-
-      @bulk_1 = @merchant_1.bulk_discounts.create!(percentage: 10, threshold: 10)
-      @bulk_2 = @merchant_1.bulk_discounts.create!(percentage: 25, threshold: 15)
-      @bulk_3 = @merchant_1.bulk_discounts.create!(percentage: 15, threshold: 20)
-      @bulk_4 = @merchant_2.bulk_discounts.create!(percentage: 10, threshold: 6)
     end
 
     it "Shows the total revenue for the selected invoice" do
